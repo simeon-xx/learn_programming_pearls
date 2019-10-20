0a0b92e0a40ad79e21acb73db4d7fb3f3871d875
王泽林 <kingshinefss@gmail.com>
2019-10-20 21:42:59 +0800

    项目初始化

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace learn_programming_pearls
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
